0186f2b2350a04fa08bdaaa286e23c7af5c0dbe4
BigBull69 <[slave.69@hotmail.fr]>
2023-03-31 10:27:44 +0200

    props.2

--- Pokedex/src/components/PokemonCard.jsx
+++ Pokedex/src/components/PokemonCard.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from "prop-types";
 
 function PokemonCard({pokemon}) {
     return (
@@ -8,5 +8,12 @@ function PokemonCard({pokemon}) {
         </figure>
     )
 }
+
+PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string,
+    }).isRequired,
+  };
 
 export default PokemonCard;
